80da0f14b748e4d7ec4d55f9172af63a11b08ffa
Sami Kyostila <sami.kyostila@nokia.com>
2010-11-19 16:40:42 +0200

    Verify that texture filter mode is NEAREST
    Signed-off-by: Sami Kyöstilä <sami.kyostila@nokia.com>

--- blittest.cpp
+++ blittest.cpp
@@ -191,4 +191,10 @@ void BlitTest::initializeBlitter()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     ASSERT_GL();
+
+    GLint read;
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &read);
+    assert(read == GL_NEAREST);
+    glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, &read);
+    assert(read == GL_NEAREST);
 }
